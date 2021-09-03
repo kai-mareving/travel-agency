ab387f223e18fbf04a2368fd845f3b45819bcb93
Kai Mareving <karska.joanna@gmail.com>
2021-09-03 17:04:28 +0200

    18.3 OrderOptionIcons v.0.1

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -5,24 +5,28 @@ import Icon from '../../common/Icon/Icon';
 import { formatPrice } from '../../../utils/formatPrice';
 
 const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) => {
-  // console.log('OrderOptionIcons: ', currentValue);
   return (
     <div className={styles.component}>
+
       {required ? '' : ( //* if false then render empty choice field
-        <div className={styles.icon} key='null' value='' onClick={setOptionValue('')}>
-          <Icon name={'times-circle'}>none</Icon>
+        <div
+          className={currentValue === '' ? styles.iconActive : styles.icon}
+          onClick={() => setOptionValue('')}
+        >
+          <Icon name={'times-circle'} />none
         </div>
       )}
+
       {values.map(value => (
         <div
-          className={styles.icon}
+          className={currentValue === value.id ? styles.iconActive : styles.icon}
           key={value.id}
-          value={currentValue}
-          onClick={value => setOptionValue(value.id)}
+          onClick={() => setOptionValue(value.id)}
         >
           <Icon name={value.icon} />{value.name} ({formatPrice(value.price)})
         </div>
       ))}
+
     </div>
   );
 };
