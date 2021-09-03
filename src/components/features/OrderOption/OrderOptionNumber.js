f9c8827197f3317ac405bdca663b08d9ef334db7
Kai Mareving <karska.joanna@gmail.com>
2021-09-03 17:37:13 +0200

    18.3 OrderOptionNumber v.0.0

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionNumber = ({currentValue}) => (
-  <div>
-    OrderOptionNumber: {currentValue}
-  </div>
-);
+const OrderOptionNumber = ({ currentValue, setOptionValue, limits, price }) => {
+  return (
+    <div className={styles.number}>
+      <input
+        type='number'
+        className={styles.inputSmall}
+        value={currentValue}
+        min={limits.min}
+        max={limits.max}
+        onChange={e => setOptionValue(parseInt(e.currentTarget.value))}
+      >
+      </input>
+      ({formatPrice(price)})
+    </div>
+  );
+};
 
 OrderOptionNumber.propTypes = {
-  currentValue: PropTypes.node.isRequired,
+  currentValue: PropTypes.number,
+  setOptionValue: PropTypes.func,
+  limits: PropTypes.object,
+  price: PropTypes.string,
+  formatPrice: PropTypes.func,
 };
 
 export default OrderOptionNumber;
