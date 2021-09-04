2fff9f138735e447fe3316d7d0ea5aa339e1004c
Kai Mareving <karska.joanna@gmail.com>
2021-09-05 01:30:48 +0200

    18.3 OrderOptionText [done]

--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
-// import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionText = ({ currentValue }) => {
+const OrderOptionText = ({currentValue, setOptionValue, type, name, id}) => {
+
   return (
     <div className={styles.component}>
-    OrderOptionText: {currentValue}
+      <input
+        type={type}
+        className={styles.input}
+        id={id}
+        value={currentValue}
+        placeholder={name}
+        onChange={e => setOptionValue(e.currentTarget.value)}
+      />
     </div>
   );
 };
 
 OrderOptionText.propTypes = {
   currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default OrderOptionText;
